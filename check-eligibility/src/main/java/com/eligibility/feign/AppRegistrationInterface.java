5d4427c58e23b58aa320505524f6737685cf0464
Triveni-2526 <boppuditriveni1410@gmail.com>
2025-05-30 22:16:46 +0530

    Add files via upload

---
+++ check-eligibility/src/main/java/com/eligibility/feign/AppRegistrationInterface.java
@@ -0,0 +1,16 @@
+package com.eligibility.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.eligibility.binding.CitizenData;
+
+
+@FeignClient(name="app-registration",url = "http://localhost:8084")
+public interface AppRegistrationInterface {
+
+	@GetMapping("/citizen/{appId}")
+	public CitizenData findCitizenById(@PathVariable Integer appId);
+	
+}
